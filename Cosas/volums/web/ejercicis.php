b7897bdd0953d4ca781c13f074a2475231d571c0
xJose12 <joseantonio022002@gmail.com>
2023-10-25 16:39:12 +0200

    Montaje de Docker y Pruebas

---
+++ Cosas/volums/web/ejercicis.php
@@ -0,0 +1,136 @@
+<!-- 1. Calculi i mostri per pantalla la suma de dos nombres.  -->
+
+<!DOCTYPE html>
+<html>
+<body>
+<h2>1. Calculi i mostri per pantalla la suma de dos nombres.</h2>
+<?php
+    $numero1 = 3;
+    $numero2 = 5;
+
+    echo "El numero 1 es " . $numero1 . "<br>" ;
+    echo "El numero 2 es " . $numero2 . "<br>";
+    $suma = $numero1 + $numero2;
+    echo "La suma de " . $numero1 . " y " . $numero2 . " es " . $suma . "<br>"; 
+    echo "La suma de $numero1 y $numero2 es $suma";
+?>
+
+<!-- 2. Calculi l’àrea d’un cercle.  -->
+
+<h2>2. Calculi l’àrea d’un cercle.</h2>
+
+<?php
+    $pi = 3.14;
+    $radio = 6;
+    $calcRadio = $pi * $radio * $radio;
+    echo "El area de un circulo con $radio de radio es $calcRadio" 
+?>
+
+<!-- 3. Calculi els graus fahrenheit corresponents a una quantitat de graus centígrads. -->
+
+<h2>3. Calculi els graus fahrenheit corresponents a una quantitat de graus centígrads.</h2>
+
+<?php
+    $gradosC = 42;
+    $calcGrados = ($gradosC * 1.8) + 32;
+    echo "Tenemos $gradosC ºC que son $calcGrados ºF"
+?>
+
+<!-- 4. Concateni dos strings i tregui per pantalla el nombre de caràcters de l’string resultats.  -->
+
+<h2>4. Concateni dos strings i tregui per pantalla el nombre de caràcters de l’string resultats.</h2>
+
+<?php
+    $palabra1 = "Hola"; 
+    $palabra2 = "Jose";
+
+    echo "La primera palabra es $palabra1 <br>";
+    echo "La Segunda palabra es $palabra2 <br>";
+    $juntar = $palabra1 . " " . $palabra2;
+    $contarCarac = strlen($palabra1 . $palabra2);
+    echo "Si las juntamos quedarian así: $juntar <br>";
+    echo "El numero total de caracteres es: $contarCarac";
+?>
+
+<!-- 5. Calculi el màxim de dos nombres i el mínim de dos nombres.  -->
+
+<h2>5. Calculi el màxim de dos nombres i el mínim de dos nombres.</h2>
+
+<?php
+    $num1 = 7;
+    $num2 = 3;
+    $num3 = 543;
+    $num4 = 432;
+
+    echo "Numeros: $num1 y $num2 <br>"; 
+    if ($num1 <=> $num2) {
+        echo "$num1 es mayor que $num2 <br>";
+    } else {
+        echo "$num2 es mayor que $num1 <br>";
+    };
+    echo "<br>";
+    echo "Numeros: $num3 y $num4 <br>"; 
+    
+    if ($num3 < $num4) {
+        echo "$num3 es menor que $num4";
+    } else {
+        echo "$num4 es menor $num3";
+    }; 
+?>
+
+<!--6. Calcula el preu total a pagar d’unes entrades de cinema en funció del nombre d’entrades. 
+Si el número d’entrades és inferior a 5 el preu de l’entrada és 5 euros. Si el nombre d’entrades està entre 5 i 9 
+ambdós inclosos el preu de l’entrades és 4 euros. I finalment si el nombre d’entrades és igual o superior a 10 el 
+preu de l’entrada és de 3 euros.  -->
+
+<h2>6. Calcula el preu total a pagar d’unes entrades de cinema en funció del nombre d’entrades. 
+Si el número d’entrades és inferior a 5 el preu de l’entrada és 5 euros. Si el nombre d’entrades està entre 5 i 9 
+ambdós inclosos el preu de l’entrades és 4 euros. I finalment si el nombre d’entrades és igual o superior a 10 el 
+preu de l’entrada és de 3 euros.</h2>
+
+<?php
+    $numeroEntradas = 25;
+
+    if ($numeroEntradas < 5) {
+        $precio = $numeroEntradas * 5;
+        echo "Tienes $numeroEntradas a 5 € que son: $precio €";
+    } elseif ($numeroEntradas > 4 && $numeroEntradas < 10) {
+        $precio = $numeroEntradas * 4;
+        echo "Tienes $numeroEntradas a 4 € que son: $precio €";
+    } else {
+        $precio = $numeroEntradas * 3;
+        echo "Tienes $numeroEntradas a 3 € que son: $precio €";
+    };
+
+?>
+
+<!--7. Mostra per pantalla un missatge que digui La persona és adulta, la persona és menor d’edat en funció de l’edat d’una persona.  -->
+
+<h2>7. Mostra per pantalla un missatge que digui La persona és adulta, la persona és menor d’edat en funció de l’edat d’una persona.</h2>
+
+<?php
+    $edad = 18;
+    echo "La persona tiene $edad años <br>";
+    if ($edad >= 18) {
+        echo "La persona es adulta";
+    } else {
+        echo "La persona es menor";
+    };
+?>
+
+<!--8. Ha d’endevinar el nombre entre 1 i 100 que l’usuari ha introduït. Pensa alguna forma de fer-lo més òptim que no sigui la de recórrer 
+tots els nombres de l’1 al 100. -->
+
+<h2>8. Ha d’endevinar el nombre entre 1 i 100 que l’usuari ha introduït. Pensa alguna forma de fer-lo més òptim que no sigui la de recórrer 
+tots els nombres de l’1 al 100.</h2>
+
+<?php
+    $numIntroducido = 3;
+    $numAdivinar = 45;
+
+
+?>
+
+
+</body>
+</html>
