7e3f3745f45e9aa17e969ad35b0929b12ed0d4bf
xJose12 <joseantonio022002@gmail.com>
2023-11-23 17:52:35 +0100

    Update funciones.php

--- Cosas/volums/web/PROYECTO/funciones.php
+++ Cosas/volums/web/PROYECTO/funciones.php
@@ -4,16 +4,16 @@
 function referencias_menu()
 {
     echo '<a href="proyecto.php"> Pagina principal</a><br>';
-    echo '<a href="funcion1.php"> Funcion 1</a><br>';
-    echo '<a href="funcion2.php"> Funcion 2</a><br>';
-    echo '<a href="funcion3.php"> Funcion 3</a><br>';
-    echo '<a href="funcion4.php"> Funcion 4</a><br>';
-    echo '<a href="funcion5.php"> Funcion 5</a><br>';
-    echo '<a href="funcion6.php"> Funcion 6</a><br>';
-    echo '<a href="funcion7.php"> Funcion 7</a><br>';
-    echo '<a href="funcion8.php"> Funcion 8</a><br>';
-    echo '<a href="funcion9.php"> Funcion 9</a><br>';
-    echo '<a href="funcion10.php"> Funcion 10</a><br>';
+    echo '<a href="pagina1.php"> Funcion 1</a><br>';
+    echo '<a href="pagina2.php"> Funcion 2</a><br>';
+    echo '<a href="pagina3.php"> Funcion 3</a><br>';
+    echo '<a href="pagina4.php"> Funcion 4</a><br>';
+    echo '<a href="pagina5.php"> Funcion 5</a><br>';
+    echo '<a href="pagina6.php"> Funcion 6</a><br>';
+    echo '<a href="pagina7.php"> Funcion 7</a><br>';
+    echo '<a href="pagina8.php"> Funcion 8</a><br>';
+    echo '<a href="pagina9.php"> Funcion 9</a><br>';
+    echo '<a href="pagina10.php"> Funcion 10</a><br>';
 }
 
 // FUNCION CARGA DE JSON DE LOS VIDEOJUEGOS
