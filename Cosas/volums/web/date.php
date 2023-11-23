ead441177745136436fa6f166dbf28666f9b4eda
xJose12 <joseantonio022002@gmail.com>
2023-11-23 15:57:45 +0100

    Update date.php

--- Cosas/volums/web/date.php
+++ Cosas/volums/web/date.php
@@ -19,7 +19,6 @@ echo "Hora en format 12h afegint “AM” o “PM: " . date("h a") . "<br>";
 <h2>2. Escriu una funció en PHP que tradueixi la data i hora d’avui al català de manera automàtica, seguint el format de l’exemple: Divendres, 4 d’agost del 2023 a les 19 hores 15 minuts i 00 segons</h2>
 
 <?php
-
 function decirFechaActual() {
 
     $dias = [
@@ -54,10 +53,50 @@ function decirFechaActual() {
 }
 
 decirFechaActual();
+?>
 
-// echo date("l") . ", " . date("d") . " de " . date("F") . " del " . date("Y") . " a las ". date("H") . " horas " . date("i") . " minutos i " . date("s") . " segundos". "<br>"; 
-// echo "Divendres, 4 d’agost del 2023 a les 19 hores 15 minuts i 00 segons"
+<!-- 3. Reescriu la funció anterior perquè tradueixi al català una data qualsevol passada per paràmetre com a “timestamp” -->
+<h2>3. Reescriu la funció anterior perquè tradueixi al català una data qualsevol passada per paràmetre com a “timestamp”</h2>
 
+<?php
+function fechaConTimeStamp(&$timestamp)
+{
+
+    $dias = [
+        "Monday" => "Lunes",
+        "Tuesday" => "Martes",
+        "Wednesday" => "Miércoles",
+        "Thursday" => "Jueves",
+        "Friday" => "Viernes",
+        "Saturday" => "Sábado",
+        "Sunday" => "Domingo"
+    ];
+    $meses = array(
+        "January" => "Enero",
+        "February" => "Febrero",
+        "March" => "Marzo",
+        "April" => "Abril",
+        "May" => "Mayo",
+        "June" => "Junio",
+        "July" => "Julio",
+        "August" => "Agosto",
+        "September" => "Septiembre",
+        "October" => "Octubre",
+        "November" => "Noviembre",
+        "December" => "Diciembre"
+    );
+
+    $dia = date("l", $timestamp);
+
+    $mes = date("F", $timestamp);
+
+    echo $dias[$dia] . ", " . date("d", $timestamp) . " de " . $meses[$mes] . " del " . date("Y", $timestamp) . " a las " . date("H", $timestamp) . " horas " . date("i", $timestamp) . " minutos i " . date("s", $timestamp) . " segundos" . "<br>";
+}
+
+$timestamp = "2012-12-13 12:12:12";
+$timestamp = strtotime($timestamp);
+
+fechaConTimeStamp($timestamp);
 ?>
 
 </html>
