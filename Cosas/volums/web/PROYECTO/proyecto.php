4c90a83d152ada1cfedd324efafa21fc4392b54e
xAna0x <anamariaac219@gmail.com>
2023-11-23 16:51:35 +0100

    modified:   Cosas/volums/web/PROYECTO/proyecto.php

--- Cosas/volums/web/PROYECTO/proyecto.php
+++ Cosas/volums/web/PROYECTO/proyecto.php
@@ -6,16 +6,16 @@
 <body>
     <?php
     function referencias_menu()
-        echo "<a href="funcion1.php"> Funcion 1</a>"
-        echo "<a href="funcion2.php"> Funcion 2</a>"
-        echo "<a href="funcion3.php"> Funcion 3</a>"
-        echo "<a href="funcion4.php"> Funcion 4</a>"
-        echo "<a href="funcion5.php"> Funcion 5</a>"
-        echo "<a href="funcion6.php"> Funcion 6</a>"
-        echo "<a href="funcion7.php"> Funcion 7</a>"
-        echo "<a href="funcion8.php"> Funcion 8</a>"
-        echo "<a href="funcion9.php"> Funcion 9</a>"
-        echo "<a href="funcion10.php"> Funcion 10</a>"
+        echo "<a href="funcion1.php"> Funcion 1</a>";
+        echo "<a href="funcion2.php"> Funcion 2</a>";
+        echo "<a href="funcion3.php"> Funcion 3</a>";
+        echo "<a href="funcion4.php"> Funcion 4</a>";
+        echo "<a href="funcion5.php"> Funcion 5</a>";
+        echo "<a href="funcion6.php"> Funcion 6</a>";
+        echo "<a href="funcion7.php"> Funcion 7</a>";
+        echo "<a href="funcion8.php"> Funcion 8</a>";
+        echo "<a href="funcion9.php"> Funcion 9</a>";
+        echo "<a href="funcion10.php"> Funcion 10</a>";
     ?>
 </body>
 </html>
